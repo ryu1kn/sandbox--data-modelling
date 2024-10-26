dc742f0451259715643ac4449048f6447d77f0c5
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2024-10-27 10:54:31 +1100

    Converted them to a macro

--- macro-lib/macros/merge_timeline.sql
+++ macro-lib/macros/merge_timeline.sql
@@ -1,42 +1,26 @@
-{% macro merge_timeline(relation, partition_by, unique_columns) %}
-with Periods as (
-    select
-        id,
-        val,
-        category,
-        valid_from,
-        valid_to,
-        -- Identify the start of a new category of adjacent periods
-        case
-            when valid_from = LAG(valid_to) over (partition by id, val, category order by valid_from) then 0
-            else 1
-        end as is_new_category
-    from
-        {{ relation }}
-),
-categorys as (
-    select
-        id,
-        val,
-        category,
-        valid_from,
-        valid_to,
-        -- Create a category identifier based on the cumulative sum of new categorys
-        sum(is_new_category) over (partition by id, val, category order by valid_from) as grp
-    from
-        Periods
-)
+{% macro merge_timeline(relation, partition_by) %}
+{% set partition_by_str = partition_by | join(', ') %}
+
+with
+    periods_w_new_group_flag as (
+        select
+            *,
+            -- Identify the start of a new category of adjacent periods
+            if(valid_from = lag(valid_to) over (partition by {{ partition_by_str }} order by valid_from), 0, 1) as is_new_group
+        from
+            {{ relation }}
+    ),
+    periods_w_group_id as (
+        select
+            * exclude is_new_group,
+            -- Create a group id based on the cumulative sum of new group
+            sum(is_new_group) over (partition by {{ partition_by_str }} order by valid_from) as group_id
+        from periods_w_new_group_flag
+    )
 select
-    id,
-    val,
-    category,
+    {{ partition_by_str }},
     min(valid_from) as valid_from,
     max(valid_to)   as valid_to
-from categorys
-group by id,
-    val,
-    category,
-    grp
-order by id,
-    valid_from
+from periods_w_group_id
+group by {{ partition_by_str }}, group_id
 {% endmacro %}
--- macro-test/models/merge_timeline/merge_timeline_model.sql
+++ macro-test/models/merge_timeline/merge_timeline_model.sql
@@ -1,5 +1,4 @@
 {{ macro_lib.merge_timeline(
     relation=source('src_merge_timeline_model', 'numbers'),
-    partition_by='category',
-    unique_columns=['val']
+    partition_by=['id', 'category', 'val'],
 ) }}
