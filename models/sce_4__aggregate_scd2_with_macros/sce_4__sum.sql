f9ba8b8d23815a681252a55842731cc5984d4965
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2024-11-03 14:30:15 +1100

    Use reslice / merge timeline macro to simplify scenario 2 implementation

--- models/sce_4__aggregate_scd2_with_macros/sce_4__sum.sql
+++ models/sce_4__aggregate_scd2_with_macros/sce_4__sum.sql
@@ -1,8 +1,8 @@
 with
     records_re_sliced_w_new_timeline as (
-        {{ macro_lib.reslice_timeline(ref('sce_2__table_scd2'), 'group_id') }}
+        {{ macro_lib.reslice_timeline(ref('sce_2__table_scd2'), ['group_id']) }}
     ),
-    final as (
+    aggregated as (
         select
             group_id,
             sum(val) as summed_val,
@@ -11,5 +11,8 @@ with
         from records_re_sliced_w_new_timeline
         group by all
         order by group_id, valid_from
+    ),
+    final as (
+        {{ macro_lib.merge_timeline('aggregated', ['group_id', 'summed_val']) }}
     )
 select * from final
