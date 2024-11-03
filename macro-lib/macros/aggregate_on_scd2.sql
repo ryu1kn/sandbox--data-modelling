7738788e331cfd86916c57886eafc9e293b7b836
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2024-11-03 21:53:59 +1100

    Make aggregate_on_scd2 macro dumb to just accept whatever `select`
    so that it can work with not just group_by but also window function

--- macro-lib/macros/aggregate_on_scd2.sql
+++ macro-lib/macros/aggregate_on_scd2.sql
@@ -1,4 +1,4 @@
-{% macro aggregate_on_scd2(relation, group_by, aggregate_expressions) %}
+{% macro aggregate_on_scd2(relation, group_by, transform_scd1, repartition_by) %}
 {% set new_columns = aggregate_expressions | map(attribute=1) | list %}
 
 with
@@ -6,22 +6,10 @@ with
         {{ macro_lib.reslice_timeline(relation, partition_by=group_by) }}
     ),
     aggregated as (
-        select
-            {{ group_by | join(', ') }},
-            {% for exp in aggregate_expressions -%}
-            {{ exp[0] }} as {{ exp[1] }},
-            {% endfor %}
-            valid_from,
-            valid_to
-        from
-            records_re_sliced_w_new_timeline
-        group by
-            {{ group_by | join(', ') }},
-            valid_from,
-            valid_to
+        {{ transform_scd1 }}
     ),
     final as (
-        {{ macro_lib.merge_timeline('aggregated', partition_by=group_by + new_columns) }}
+        {{ macro_lib.merge_timeline('aggregated', partition_by=group_by + repartition_by) }}
     )
 select * from final
 
